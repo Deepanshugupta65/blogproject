e6fb7b8b528f777cb0ea758d03876dea7f0e093e
Deepanshugupta65 <dj911743@gmail.com>
2023-12-06 10:56:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  con
+  return (
+    <>
+      <h1>app irfjiof</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);    
+    }
+
+    // account
+    async createAccount({email,password,name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+            // if useraccount is exist then login 
+            if(userAccount){
+            //   call another method
+            return this.login({email,password});
+            }
+            else{
+                return userAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+    }
+    // login
+    async login({email,password}){
+        try{
+           return await this.account.createEmailSession(email,password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    // login hain ya nahi , we are in home page
+    async getCurrentUser(){
+        try {
+           return await this.account.get();
+        } catch (error) {
+            console.log("appwrite service",error)
+        }
+        return null;
+    }
+    // logout
+    async logout(){
+        try{
+          return this.account.deleteSessions();
+        }catch(error){
+            console.log("error",error)
+        }
+    }
+}
+
+const authService = new AuthService();
+export default AuthService;
