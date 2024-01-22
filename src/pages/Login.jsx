3eb2e95b03727bae9bf6414d4780e61e77c4ce01
Deepanshugupta65 <dj911743@gmail.com>
2024-01-22 14:27:12 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Login as loginComponent} from '../components'
+import {Login as loginComponent} from "../components"
 function Login() {
   return (
     <div className='py-8'>
