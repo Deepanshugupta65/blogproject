b8197ac50cd85afecedb0a34161c95985dd339b1
Deepanshugupta65 <dj911743@gmail.com>
2024-02-09 12:05:14 +0530

    post form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../Logo'
+// import Logo from '../Logo'
 
 function Footer() {
   return (
@@ -9,9 +9,9 @@ function Footer() {
                 <div className="-m-6 flex flex-wrap">
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                         <div className="flex h-full flex-col justify-between">
-                            <div className="mb-4 inline-flex items-center">
+                            {/* <div className="mb-4 inline-flex items-center">
                                 <Logo width="100px" />
-                            </div>
+                            </div> */}
                             <div>
                                 <p className="text-sm text-gray-600">
                                     &copy; Copyright 2023. All Rights Reserved by DevUI.
--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,21 +1,23 @@
-import React,{useCallback} from 'react'
-import {useForm} from "react-hook-form"
-import {Button , Input ,Select , RTE} from '../index';
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, RTE, Select } from "..";
 import appwriteService from "../../appwrite/config";
-import { useNavigate } from 'react-router-dom';
-import  {useSelector }  from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default function PostForm({post}) {
-    const {register,handleSubmit,watch,setValue,control,getValues} = useForm({
-        defaultValues:{
+export default function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
             title: post?.title || "",
             slug: post?.$id || "",
             content: post?.content || "",
             status: post?.status || "active",
         },
-    })
-    const navigate = useNavigate()
-    const userData = useSelector(state => state.auth.userData)
+    });
+
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+
     const submit = async (data) => {
         if (post) {
             const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
@@ -46,6 +48,7 @@ export default function PostForm({post}) {
             }
         }
     };
+
     const slugTransform = useCallback((value) => {
         if (value && typeof value === "string")
             return value
@@ -67,56 +70,53 @@ export default function PostForm({post}) {
         return () => subscription.unsubscribe();
     }, [watch, slugTransform, setValue]);
 
-
-  return (
-    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-    <div className="w-2/3 px-2">
-        <Input
-            label="Title :"
-            placeholder="Title"
-            className="mb-4"
-            {...register("title", { required: true })}
-        />
-        <Input
-            label="Slug :"
-            placeholder="Slug"
-            className="mb-4"
-            {...register("slug", { required: true })}
-            onInput={(e) => {
-                setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
-            }}
-        />
-        <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
-    </div>
-    <div className="w-1/3 px-2">
-        <Input
-            label="Featured Image :"
-            type="file"
-            className="mb-4"
-            accept="image/png, image/jpg, image/jpeg, image/gif"
-            {...register("image", { required: !post })}
-        />
-        {post && (
-            <div className="w-full mb-4">
-                <img
-                    src={appwriteService.getFilePreview(post.featuredImage)}
-                    alt={post.title}
-                    className="rounded-lg"
+    return (
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
                 />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
-        )}
-        <Select
-            options={["active", "inactive"]}
-            label="Status"
-            className="mb-4"
-            {...register("status", { required: true })}
-        />
-        <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
-            {post ? "Update" : "Submit"}
-        </Button>
-    </div>
-</form>
-)
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={appwriteService.getFilePreview(post.featuredImage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
+    );
 }
-
-// export default PostForm
